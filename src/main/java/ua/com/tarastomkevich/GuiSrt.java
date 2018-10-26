f47177a2203ee994b7fd3774e77a1eb3840e588a
HeavyMetal <ttm2006@ukr.net>
2018-10-26 16:54:56 +0300

    'write_result_in_file'

--- src/main/java/ua/com/tarastomkevich/GuiSrt.java
+++ src/main/java/ua/com/tarastomkevich/GuiSrt.java
@@ -1,5 +1,7 @@
 package ua.com.tarastomkevich;
 
+import ua.com.tarastomkevich.util.FilterFileSubtitle;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,21 +9,22 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
-import ua.com.tarastomkevich.util.*;
-
 
 public class GuiSrt extends JFrame {
+    public static String newNameFile;
+
     public GuiSrt() throws HeadlessException {
         initFrame();
     }
 
     private void initFrame() {
-        setSize(new Dimension(600, 120));
+        setSize(new Dimension(600, 110));
         setTitle("Subtitle Extractor");
+        setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         JPanel panel = new JPanel();
-        JLabel label = new JLabel("Скористайтесь кнопкою Open та відкрийте один файл субтитрів. Вивід буде здійснений у консоль.");
+        JLabel label = new JLabel("Скористайтесь кнопкою Open та відкрийте один файл субтитрів.Вивід буде здійснений у консоль.");
         panel.add(label);
         JButton buttonOpen = new JButton("Open");
         buttonOpen.addActionListener(new ActionListener() {
@@ -32,21 +35,20 @@ public class GuiSrt extends JFrame {
                 fileChooser.setFileFilter(new FilterFileSubtitle());
                 int ret = fileChooser.showDialog(panel, "Open file subtitle");
                 if (ret == JFileChooser.APPROVE_OPTION) {
-                    File file = fileChooser.getSelectedFile();
                     if(!fileChooser.getSelectedFile().getName().endsWith("srt")){
                         JOptionPane.showMessageDialog(panel,
                                 new String[] {"Ви вибрали неправильний файл: ",
                                         " - спробуйте знайти файл srt"},
                                 "Wrong type file",
                                 JOptionPane.ERROR_MESSAGE);
-
                     } else {
                         try {
-                            SimpleSrt.pathFileSrt = file.getCanonicalPath();
+                            SimpleSrt.pathFileSrt = fileChooser.getSelectedFile().getCanonicalPath();
+                            newNameFile = fileChooser.getCurrentDirectory() + "\\new" + fileChooser.getSelectedFile().getName();
+                            SimpleSrt.extractSrt();
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                        SimpleSrt.extractSrt();
                     }
                 }
             }
--- src/main/java/ua/com/tarastomkevich/SimpleSrt.java
+++ src/main/java/ua/com/tarastomkevich/SimpleSrt.java
@@ -1,25 +1,39 @@
 package ua.com.tarastomkevich;
 
 import java.io.*;
+import static ua.com.tarastomkevich.GuiSrt.newNameFile;
 
 public class SimpleSrt {
     public static String pathFileSrt;
+    public static String strTemp;
+    public static File fileSrt;
 
     public static void extractSrt() {
         int countStr = 0;
-        try (InputStream inputStream = new FileInputStream(pathFileSrt)){
+        fileSrt = new File(newNameFile);
+        try (InputStream inputStream = new FileInputStream(pathFileSrt);
+             OutputStream outputStream = new FileOutputStream(fileSrt)){
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+            System.out.println(fileSrt.getCanonicalPath());
             while (true) {
-                String strTemp;
                     if((strTemp = bufferedReader.readLine())!= null){
                             if (!strTemp.isEmpty()&&(countStr++%3==2)) {
-                                System.out.println(strTemp);
+                                int c;
+                                while (true){
+                                    for (int i = 0; i < strTemp.length(); i++) {
+                                        c = strTemp.charAt(i);
+                                        bufferedWriter.write((char)c);
+                                    }
+                                    bufferedWriter.newLine();
+                                    bufferedWriter.flush();
+                                    break;
+                                }
                             }
                     }else break;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            System.out.println(pathFileSrt);
         } catch (IOException e) {
             System.out.println("File opening failed");
             e.printStackTrace();
